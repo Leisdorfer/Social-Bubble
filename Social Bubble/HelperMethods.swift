81c480546241e1a12f0c980d870f66f8005adc16
Lauren Eisdorfer <leisdorfer67@gmail.com>
2017-08-14 09:12:54 -0500

    Initial commit

---
+++ Social Bubble/HelperMethods.swift
@@ -0,0 +1,9 @@
+//
+//  HelperMethods.swift
+//  Social Bubble
+//
+//  Created by Eisdorfer, Lauren on 8/14/17.
+//  Copyright © 2017 Eisdorfer, Lauren. All rights reserved.
+//
+
+import Foundation
