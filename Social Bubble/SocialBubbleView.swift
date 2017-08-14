2f6f63b86d6c008267a42b6f9a9c383740e3deef
Lauren Eisdorfer <leisdorfer67@gmail.com>
2017-08-14 13:29:30 -0500

    Non-intersecting bubbles

--- Social Bubble/SocialBubbleView.swift
+++ Social Bubble/SocialBubbleView.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class SocialBubbleView: UIView {
     private let title = UILabel()
-    
+    private var previousBubbles = [UIView]()
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .black
@@ -17,26 +17,38 @@ class SocialBubbleView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        addRandomBubbles()
+        defer { addSubview(title) }
         let contentArea = bounds.insetBy(dx: Padding.large, dy: Padding.large)
         let titleSize = title.sizeThatFits(contentArea.size)
         title.frame = CGRect(x: contentArea.midX - titleSize.width/2, y: contentArea.minY, size: titleSize)
-        addSubview(title)
+        addRandomBubbles()
     }
     
     private func addRandomBubbles() {
-        (0...5).forEach { _ in
+        (0...15).forEach { _ in
             let diameter = CGFloat(arc4random_uniform(100) + 50)
             let x = CGFloat(arc4random_uniform(UInt32(bounds.maxX)))
-            let y = CGFloat(arc4random_uniform(UInt32(bounds.maxY)))
-            let bubble = UIView(frame: CGRect(x: x - diameter, y: y - diameter, width: diameter, height: diameter))
-            bubble.backgroundColor = UIColor(hue: CGFloat(arc4random_uniform(100))/100.0, saturation: 1.0, brightness: 1.0, alpha: 1.0)
-            bubble.layer.cornerRadius = diameter/2
-            bubble.layer.shadowColor = UIColor.white.cgColor
-            bubble.layer.shadowOffset = CGSize(width: 0, height: 0)
-            bubble.layer.shadowOpacity = 0.75
-            bubble.layer.shadowRadius = 10
-            addSubview(bubble)
+            let y = CGFloat(arc4random_uniform(UInt32(bounds.maxY)) + UInt32(title.frame.maxY))
+            let bubble = UIView(frame: CGRect(x: x - diameter, y: y, width: diameter, height: diameter))
+            let intersect = previousBubbles.reduce(false) { $0 || $1.frame.intersects(bubble.frame) }
+            if !intersect {
+                addStyle(toBubble: bubble, withDiameter: diameter)
+                addSubview(bubble)
+                previousBubbles.append(bubble)
+            }
         }
+    }
+    
+    private func addStyle(toBubble bubble: UIView, withDiameter diameter: CGFloat) {
+        bubble.backgroundColor = UIColor(hue: CGFloat(arc4random_uniform(100))/100.0, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+        addShadow(toBubble: bubble)
+        bubble.layer.cornerRadius = diameter/2
+    }
+    
+    private func addShadow(toBubble bubble: UIView) {
+        bubble.layer.shadowColor = UIColor.white.cgColor
+        bubble.layer.shadowOffset = CGSize(width: 0, height: 0)
+        bubble.layer.shadowOpacity = 0.75
+        bubble.layer.shadowRadius = 10
     }
 }
