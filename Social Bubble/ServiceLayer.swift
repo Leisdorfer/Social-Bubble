8b4bb03a62dda1b9b63ac5d0a7490aaf758c138b
Lauren Eisdorfer <leisdorfer67@gmail.com>
2017-08-15 12:20:44 -0500

    Add Facebook framework

---
+++ Social Bubble/ServiceLayer.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+class ServiceLayer {
+    //search?q=St.Louis,STL&type=event
+}
--- Social Bubble/SocialBubbleView.swift
+++ Social Bubble/SocialBubbleView.swift
@@ -1,14 +1,20 @@
 import UIKit
+import FacebookLogin
+import FacebookCore
+import FacebookShare
 
 class SocialBubbleView: UIView {
     private let title = UILabel()
-    private var previousBubbles = [UIView]()
+    private let login = LoginButton(readPermissions: [.publicProfile])
+    fileprivate var visibleBubbles = [UIButton]()
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .black
+        addShadow(toView: title, withRadius: 4)
         title.textColor = .white
         title.text = "Social Bubble"
         title.font = UIFont.systemFont(ofSize: 54)
+        addSubview(login)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -21,34 +27,53 @@ class SocialBubbleView: UIView {
         let contentArea = bounds.insetBy(dx: Padding.large, dy: Padding.large)
         let titleSize = title.sizeThatFits(contentArea.size)
         title.frame = CGRect(x: contentArea.midX - titleSize.width/2, y: contentArea.minY, size: titleSize)
+        let loginSize = CGSize(width: 88, height: 44)
+        login.frame = CGRect(x: contentArea.midX - loginSize.width/2, y: title.frame.maxY, size: loginSize)
         addRandomBubbles()
+        //addAnimation(toBubbles: visibleBubbles)
     }
     
-    private func addRandomBubbles() {
+    fileprivate func addRandomBubbles() {
         (0...15).forEach { _ in
             let diameter = CGFloat(arc4random_uniform(100) + 50)
             let x = CGFloat(arc4random_uniform(UInt32(bounds.maxX)))
-            let y = CGFloat(arc4random_uniform(UInt32(bounds.maxY)) + UInt32(title.frame.maxY))
-            let bubble = UIView(frame: CGRect(x: x - diameter, y: y, width: diameter, height: diameter))
-            let intersect = previousBubbles.reduce(false) { $0 || $1.frame.intersects(bubble.frame) }
+            let y = CGFloat(arc4random_uniform(UInt32(bounds.maxY)) + UInt32(login.frame.maxY))
+            let bubble = UIButton(frame: CGRect(x: x - diameter, y: y, width: diameter, height: diameter))
+            let intersect = visibleBubbles.reduce(false) { $0 || $1.frame.intersects(bubble.frame) }
             if !intersect {
                 addStyle(toBubble: bubble, withDiameter: diameter)
                 addSubview(bubble)
-                previousBubbles.append(bubble)
+                visibleBubbles.append(bubble)
             }
         }
     }
     
-    private func addStyle(toBubble bubble: UIView, withDiameter diameter: CGFloat) {
+    fileprivate func addStyle(toBubble bubble: UIButton, withDiameter diameter: CGFloat) {
         bubble.backgroundColor = UIColor(hue: CGFloat(arc4random_uniform(100))/100.0, saturation: 1.0, brightness: 1.0, alpha: 1.0)
-        addShadow(toBubble: bubble)
+        addShadow(toView: bubble, withRadius: 10)
         bubble.layer.cornerRadius = diameter/2
     }
     
-    private func addShadow(toBubble bubble: UIView) {
-        bubble.layer.shadowColor = UIColor.white.cgColor
-        bubble.layer.shadowOffset = CGSize(width: 0, height: 0)
-        bubble.layer.shadowOpacity = 0.75
-        bubble.layer.shadowRadius = 10
+    fileprivate func addAnimation(toBubbles bubbles: [UIButton]) {
+        UIView.animate(withDuration: 10, animations: {
+            bubbles.forEach { [weak self] bubble in
+                guard let `self` = self else { return }
+                let x = CGFloat(arc4random_uniform(UInt32(self.bounds.maxX)))
+                let y = CGFloat(arc4random_uniform(UInt32(self.bounds.maxY)))
+                var bubbleFrame = bubble.frame
+                if bubbleFrame.maxX < self.bounds.maxX && bubbleFrame.maxY < self.bounds.maxY {
+                    bubbleFrame.origin.x += x
+                    bubbleFrame.origin.y += y
+                    bubble.frame = bubbleFrame
+                }
+            }
+        })
+    }
+    
+    fileprivate func addShadow(toView view: UIView, withRadius radius: CGFloat) {
+        view.layer.shadowColor = UIColor.white.cgColor
+        view.layer.shadowOffset = CGSize(width: 0, height: 0)
+        view.layer.shadowOpacity = 0.75
+        view.layer.shadowRadius = radius
     }
 }
